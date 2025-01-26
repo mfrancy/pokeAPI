1c50df32f3db49563edad1b24a8aecd358ab2c52
Mariana Lopes <marianalopes697@gmail.com>
2025-01-25 22:35:30 -0300

    Inserindo dados dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,39 +57,66 @@ async function showPokemon(nome) {
      return response.json()
 }
 
-function createHtml(detalhesPokemon) {
+function changeColor(type) {
+  let colorCardPokemon; 
+  let colorTypePokemon;
 
-    return `<li id="pokemon-list"  class="pokemon">
-          <span id="number-pokemon" class="number">#001</span>
+  if (type === 'grass') {
+    colorCardPokemon = '#49d0b0'
+     colorTypePokemon = '#2d8772'
+
+  } else if (type === 'fire') {  
+    colorCardPokemon ='#f13636e3'
+    colorTypePokemon ='#a71616e3'
+  } else if (type === 'water') {
+    colorCardPokemon = '#0b57d0'
+    colorTypePokemon = '#0b45a3'
+  } else {
+    colorCardPokemon = '#83d00b'
+    colorTypePokemon = '#5e9509'
+  }
+
+  return [colorCardPokemon, colorTypePokemon];
+}
+
+function createHtml(detalhesPokemon) {
+  const [colorPokemon, colorType] = changeColor(detalhesPokemon.types[0].type.name);
+  const typePokemon = detalhesPokemon.types[1] ? `<li style="background-color: ${colorType}" class="type">${detalhesPokemon.types[1]?.type?.name}</li>` : ''
+  // const colorPokemon = changeColor(detalhesPokemon.types[0].type.name)
+
+    return `<li id="pokemon-list" style="background-color: ${colorPokemon}";  class="pokemon">
+          <span id="number-pokemon" class="number">#00${detalhesPokemon.id}</span>
           <span id="name-pokemon" class="name">${detalhesPokemon.name}</span>
           <div class="detail">
-            <ol class="types">
-              <li id class="type">grass</li>
-              <li class="type">poison</li>
+            <ol class="types">       
+              <li class="type" style="background-color: ${colorType}">${detalhesPokemon.types[0].type.name}</li>
+              ${typePokemon}
             </ol>
             <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${detalhesPokemon.id}.svg"
               alt=""
               srcset=""
             />
           </div>
         </li>`
+        
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
     const lista = document.querySelector('ol')
-    const {results: listaPokemon} = await getListNamePokemon(3)
-    const arrayDePokemons = listaPokemon.map(async function (element) {
-        console.log(element.name)
-        const detalhesPokemon = await showPokemon(element.name)
-        return createHtml(detalhesPokemon)
-    })
-    console.log(await arrayDePokemons)
+    const {results: listaPokemon} = await getListNamePokemon(14)
+    // const arrayDePokemons = listaPokemon.map(async function (element) {
+    //     console.log(element.name)
+    //     const detalhesPokemon = await showPokemon(element.name)
+    //     return createHtml(detalhesPokemon)
+    // })
+    // console.log(await arrayDePokemons)
 
-    // for (let i = 0; i < listaPokemon.length; i++) {
-    //     const detalhesPokemon = await showPokemon(listaPokemon[i].name)
-    //     const itemHtml = createHtml(detalhesPokemon)
-    //     lista.innerHTML += itemHtml
-    // }
+    for (let i = 0; i < listaPokemon.length; i++) {
+        const detalhesPokemon = await showPokemon(listaPokemon[i].name)
+        const itemHtml = createHtml(detalhesPokemon)
+        console.log(detalhesPokemon)
+        lista.innerHTML += itemHtml
+    }
 
 })
