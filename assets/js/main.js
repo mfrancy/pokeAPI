049c07496ded658b6d8e6b72a5c2a3b804cc24ea
mfrancy <marianalopes697@gmail.com>
2025-01-27 10:55:13 -0300

    Modal de detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,19 @@
 const pokemonList = document.getElementById('pokemon-list')
 const numberPokemon = document.getElementById('number-pokemon')
 const lista = document.querySelector('ol')
-const openModalBtn = document.getElementById("openModalBtn")
+const openModalBtn = document.getElementById("name-pokemon")
+const allPokemons = document.getElementById("allPokemons")
 
+allPokemons.addEventListener('click', function (e) {
+    const btnPokemon = e.target.closest('.btnPokemon')
+    const pokemonId = btnPokemon.getAttribute('data-pokemon')
+    const modalPokemon = document.querySelector('.modal')
 
+    modalPokemon.style.display = 'block'
 
-// const pokedex = []
+    
+})
 
-// async function getUrlPokemons() {
-//     const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=5')
-//     const data = await response.json()
-
-//     for (let i = 0; i < data.results.length; i++) {
-//         console.log(data.results[i].name)
-
-//         const nomePokemon = data.results[i].name
-//         const {name} = await getPokemon(nomePokemon)
-//         console.log(name)
-
-        // const htmlPokedex = `<li id="pokemon-list"  class="pokemon">
-        //   <span id="number-pokemon" class="number">#001</span>
-        //   <span id="name-pokemon" class="name">${name}</span>
-        //   <div class="detail">
-        //     <ol class="types">
-        //       <li id class="type">grass</li>
-        //       <li class="type">poison</li>
-        //     </ol>
-        //     <img
-        //       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-        //       alt=""
-        //       srcset=""
-        //     />
-        //   </div>
-        // </li>`
-
-//         pokedex.push(htmlPokedex)
-
-//     }
-
-//     lista.innerHTML = pokedex.join(' ')
-// }
-
-// async function getPokemon(nome) {
-//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
-//     return response.json()
-// }
-
-// getUrlPokemons()
 
 async function getListNamePokemon(limite) {
     
@@ -81,6 +48,7 @@ function changeColor(type) {
   return [colorCardPokemon, colorTypePokemon];
 }
 
+
 function createHtml(detalhesPokemon) {
   const [colorPokemon, colorType] = changeColor(detalhesPokemon.types[0].type.name);
   const typePokemon = detalhesPokemon.types[1] ? `<li style="background-color: ${colorType}" class="type">${detalhesPokemon.types[1]?.type?.name}</li>` : ''
@@ -88,7 +56,9 @@ function createHtml(detalhesPokemon) {
 
     return `<li id="pokemon-list" style="background-color: ${colorPokemon}";  class="pokemon">
           <span id="number-pokemon" class="number">#00${detalhesPokemon.id}</span>
-          <span id="name-pokemon" class="name">${detalhesPokemon.name}</span>
+          <button style="background-color: ${colorType}" class="name btnPokemon" data-pokemon="${detalhesPokemon.id}">
+            ${detalhesPokemon.name}
+          </button>
           <div class="detail">
             <ol class="types">       
               <li class="type" style="background-color: ${colorType}">${detalhesPokemon.types[0].type.name}</li>
@@ -107,17 +77,12 @@ function createHtml(detalhesPokemon) {
 document.addEventListener('DOMContentLoaded', async () => {
     const lista = document.querySelector('ol')
     const {results: listaPokemon} = await getListNamePokemon(14)
-    // const arrayDePokemons = listaPokemon.map(async function (element) {
-    //     console.log(element.name)
-    //     const detalhesPokemon = await showPokemon(element.name)
-    //     return createHtml(detalhesPokemon)
-    // })
-    // console.log(await arrayDePokemons)
+
 
     for (let i = 0; i < listaPokemon.length; i++) {
         const detalhesPokemon = await showPokemon(listaPokemon[i].name)
         const itemHtml = createHtml(detalhesPokemon)
-        console.log(detalhesPokemon)
+        // console.log(detalhesPokemon)
         lista.innerHTML += itemHtml
     }
 })
